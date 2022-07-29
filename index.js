3029c651b50d7fed0dd2691e54239a4ef3173b8f
travis flake <tflake83@gmail.com>
2022-07-29 07:28:10 -0400

    csurf integration

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const path = require('path')
 const models = require('./models')
-const { user } = require('pg/lib/defaults')
+const { user, password } = require('pg/lib/defaults')
 const User = require('./models/user')
 const app = express()
 const PORT = process.env.PORT || 3000 
+const session = require('express-session');
+const { env } = require('process');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+var csurf = require('csurf')
 
 app.set('view engine', 'ejs') //PROGRAM TO CONSTRUCT HTML FILES
 app.set('views', path.join(__dirname, 'views')) //WHERE THE TEMPLATE FILE ARE AT RELATIVE TO EJS
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(
+  session({
+    secret: process.env['SESSION_SECRET'],
+    store: new SequelizeStore({
+    db: models.database,
+    }),
+    resave: false, // we support the touch method so per the express-session docs this should be set to false
+    saveUninitialized: false
+  })
+);
+app.use(csurf())
+
 app.use(async (requst, response, next) => {
     try {
         await models.database.authenticate()
-        await models.database.sync({alter:true})
+        await requst.sessionStore.sync({alter:true})
 
         console.log('authenticated success!')
     } catch(err) {
@@ -27,7 +43,7 @@ app.use(async (requst, response, next) => {
 })
 
 app.get('/signUp', async (req, res) => {
-    res.render('signUp', {message: 'hope this works'})
+    res.render('signUp', { err : false, surf: req.csrfToken()})
 })
 
 app.post('/signUp', async (req, res) => {
@@ -41,7 +57,7 @@ app.post('/signUp', async (req, res) => {
             password: hash,
         })
     } catch(err) {
-        res.render('signUp', { err })
+        res.render('signUp', { err , surf : req.csrfToken()})
         console.log(err)
         return
     } 
@@ -49,17 +65,14 @@ app.post('/signUp', async (req, res) => {
 })
 
 app.get('/logIn', async (req, res) => {
-    res.render('logIn')
+    res.render('logIn', {surf : req.csrfToken()})
 })
 app.post('/logIn', async (req, res) => { 
-    const saltRounds = 10
-    const hash = await bcrypt.hash(req.body.password, saltRounds) // HASHING PASSWORD
     const user = await User.findOne({where: { userName: req.body.userName }})
-    
-    if (hash === user.password) {
+    if ( await bcrypt.compare(req.body.password, user.password)) {
         res.send('Success')
     } else {
-        res.send(err)
+        res.send('Not a valid User')
     }
             
     
